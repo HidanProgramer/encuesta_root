97f47db619165ad24bec9da95da57a2c8716d164
HiddanProgrammer <hiddanprogrammer@gmail>
2026-05-22 16:17:48 +0200

    Paso 5: Declarar la libreria, componenete y vista del Mapa Global

--- __manifest__.py
+++ __manifest__.py
@@ -15,12 +15,21 @@
              'views/view_encuesta_actividad_inherit.xml',
              'views/view_encuesta_aspectos_inherit.xml',
              'views/view_encuesta_criterios_inherit.xml',
-             'views/view_dashboard.xml'],
+             'views/view_dashboard.xml',
+             'views/view_mapa_global.xml'],
     'assets': {
         'web.assets_backend': [
+            # Librería del Dashboard
             'encuesta_root/static/src/libs/chart.umd.js',
+            # Componente y vista Dashboard
             'encuesta_root/static/src/components/dashboard/dashboard.js',
             'encuesta_root/static/src/components/dashboard/dashboard.xml',
+            # Librería del Mapa Global
+            'encuesta_root/static/src/libs/leaflet/leaflet.css',
+            'encuesta_root/static/src/libs/leaflet/leaflet.js',
+            # Componente Mapa Global
+            'encuesta_root/static/src/components/mapa_global/mapa_global.js',
+            'encuesta_root/static/src/components/mapa_global/mapa_global.xml',
         ],
     },
     'images': ['static/description/banner.jpg'],
